1fa9e918b2099d34d5a3fbe57cc0496af2f20f0e
laisecaldeira <laise_caldeira@hotmail.com>
2022-05-22 19:08:31 -0300

    Atividade - encontro remoto 2

---
+++ Atividade - encontro remoto 2.js
@@ -0,0 +1,39 @@
+var listaDePecas = ["Amortecedor","Motor","Filtro de ar"]
+
+let peso = 50
+if(peso < 100){
+    console.log("A peça deve pesar no mínimo 100g")
+}else{
+    console.log("A peça possui peso adequado")
+}
+
+
+
+var listaDePecas = ["Amortecedor","Motor","Filtro de ar"]
+
+if(listaDePecas.length < 10){
+    console.log("Ainda há espaço")
+}else{
+    console.log("Espaço não disponível")
+}
+
+
+
+var listaDePecas = ["Amortecedor","Motor","Filtro de ar"]
+
+if(listaDePecas.length < 10){
+    console.log("Ainda há espaço")
+}else{
+    console.log("Espaço não disponível")
+}
+
+
+
+let nomePeca = "Disco de freio"
+if(nomePeca.length > 3){
+    console.log("Nome da peça está adequado")
+}else if(nomePeca.length == 0){
+    console.log("Nome da peça não pode ser vazio")
+}else {
+    console.log("Nome da peça precisa ter, no mínimo, 3 caracteres")
+}
